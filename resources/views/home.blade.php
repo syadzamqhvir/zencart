70df2c41762d2087c06c173d5f2bcfdb3468d5c4
syadzamqhvir <syalmaov28@gmail.com>
2024-08-31 09:00:36 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZenCart | E-Commerce </title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+    rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous"> -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+</head>
+<body>
+
+
+
+<div class="header">
+    
+
+
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="images/logo.png" alt="125px">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="Home">Home</a></li>
+                    <li><a href="Products">Products</a></li>
+                    <li><a href="Review"></a>Review</li>
+                    <li><a href="Contact"></a>Contact</li>
+                    <li><a href="Account"></a>Account</li>
+                    
+            </ul>
+            </nav>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                    <h1>Give Your Workout<br>A New Style</h1>
+                    <p>Success isn't always about greatness. It's about consistency. Consistent<br>hard work leads to success. Greatness will come.</p>
+            <a href="" class="btn">Explore Now &#8594; </a>
+                </div>
+            <div class="col-2">
+                <img src="images/image1.png" alt="">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<i class="fa fa-address-book-o" aria-hidden="true"></i>
+
+<!-- ------ featured categories ------- -->
+ <div class="categories">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-3">
+                <img src="images/featured 1.jpg">
+            </div>
+            <div class="col-3">
+                <img src="images/featured 2.jpg">
+            </div>
+            <div class="col-3">
+                <img src="images/featured 3.jpg">
+            </div>
+        </div>
+        <h2>Featured Products</h2>
+    </div>
+
+ </div>
+<!-- ------ featured products ------- -->
+ <div class="small-container">
+    
+    <div class="row">
+        <div class="col-4">
+            
+            <img src="images/latest 1.jpg">
+            <h4>Puma Red T-Shirt</h4>
+                <div class="rating">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star-half' ></i>
+                    <i class='bx bx-star' ></i>
+                </div>
+            <p>$50.0</p>
+        </div>
+
+        <div class="col-4">
+            <img src="images/latest 2.jpg">
+            <h4>Puma Red T-Shirt</h4>
+                <div class="rating">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star-half' ></i>
+                    <i class='bx bx-star' ></i>
+                </div>
+            <p>$50.0</p>
+        </div> 
+
+        <div class="col-4">
+            <img src="images/latest 3.jpg">
+            <h4>Puma Red T-Shirt</h4>
+                <div class="rating">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star-half' ></i>
+                    <i class='bx bx-star' ></i>
+                </div>
+            <p>$50.0</p>
+        </div>
+    </div>
+    <h2>Latest Products</h2>
+    </div>
+</div>
+
+    
+ </div>
+ <!-------offer-->
+ <div class="offer">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-2">
+                <img src="images/exclusive.png" class="offer-img">
+            </div>
+            <div class="col-2">
+               <p>Exclusively Available on ZenCart</p>
+               <h1>Smart Band 4</h1>
+               <small> The Mi Smart Band 4 features 
+                0baksjbiciabiavcuavcvauivcuavcasv</small>
+                <a href="" class="btn">Buy Now &#8594;</a>
+            </div>
+        </div>
+    </div>
+ </div>
+
+ <!------ testimonial ------>
+
+    <div class="testimonial">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                    <i class="fa fa-qoute-left" ></i>
+                    <p>dnwdbwjqibfiqbfuwndoqwbf
+                        fjefniwejbfiweubfehwbuiewbgu
+                        ebgbbuqbfebgegbowbsfbiubfuqbfiuq</p>
+                        <div class="rating">
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star-half' ></i>
+                            <i class='bx bx-star' ></i>
+                        </div>
+                        <img src="images/user-1.png">
+                        <h3>Sean Parker</h3>
+                </div>
+
+                <div class="col-3">
+                    <i class="fa fa-qoute-left" ></i>
+                    <p>dnwdbwjqibfiqbfuwndoqwbf
+                        fjefniwejbfiweubfehwbuiew
+                        bguebgbbuqbfebgegbowbsfbiubfuqbfiuq</p>
+                        <div class="rating">
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star-half' ></i>
+                            <i class='bx bx-star' ></i>
+                        </div>
+                        <img src="images/user-2.png">
+                        <h3>Mike Smith</h3>
+                </div>
+
+                <div class="col-3">
+                    <i class="fa fa-qoute-left" ></i>
+                    <p>dnwdbwjqibfiqbfuwndoqwbf
+                        fjefniwejbfiweubfehwbuiewbguebg
+                        bbuqbfebgegbowbsfbiubfuqbfiuq</p>
+                        <div class="rating">
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star'></i>
+                            <i class='bx bxs-star-half' ></i>
+                            <i class='bx bx-star' ></i>
+                        </div>
+                        <img src="images/user-3.png">
+                        <h3>Mabel Joe</h3>
+                </div>
+                <h2>Customer's Review</h2>
+            </div>
+        </div>
+    </div>
+
+   
+
+    <section id="Contact">
+        <div class="container">
+            <h2>Contact Me</h2>
+            <div class="contact-info">
+                <div class="form-group">
+                  <label for="name">Your Name</label>
+                  <input type="text" id="name" name="name" required>
+                </div>
+            </div>
+            <form id="contact-form" method="POST" action="send_email.php">
+                <div class="form-group">
+                  <label for="phone">Phone</label>
+                    <input type="number" id="phone" name="phone"  required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Your Email</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea id="message" name="message" rows="5" required></textarea>
+                </div>
+                <button type="submit">Send Message</button>
+            </form>
+        </div>
+      </section>
+</body>
+</html>
